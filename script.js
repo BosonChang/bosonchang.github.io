46d72f6498303efb956f2b925642311b5ce393a2
ShpideyBobo <boson0710@gmail.com>
2023-09-15 12:16:57 -0400

    Working nav sync

---
+++ script.js
@@ -0,0 +1,5 @@
+// Use JavaScript to include the navbar
+document.addEventListener("DOMContentLoaded", function () {
+    const navbarPlaceholder = document.getElementById("navbar-placeholder");
+    navbarPlaceholder.innerHTML = '<object data="navbar.html" width="100%" height="50"></object>';
+});
