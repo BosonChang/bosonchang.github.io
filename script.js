e4cf4267db4db3d5ca06b8b30bffee206449d034
ShpideyBobo <boson0710@gmail.com>
2023-09-16 02:56:27 -0400

    test

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Use JavaScript to include the navbar
 document.addEventListener("DOMContentLoaded", function () {
     const navbarPlaceholder = document.getElementById("navbar-placeholder");
-    navbarPlaceholder.innerHTML = '<object data="navbar.html" width="100%" height="50"></object>';
+    navbarPlaceholder.innerHTML = '<object data="navbar.html" width="100%" height="100%" style="overflow-clip-margin:0"></object>';
 });
