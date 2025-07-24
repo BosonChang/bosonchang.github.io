83ddc68e642e0f2fc22cfd4a21642f8585cfc628
ShpideyBobo <boson0710@gmail.com>
2025-07-24 14:53:19 +0800

    Clear

--- script.js
+++
@@ -1,8 +0,0 @@
-// === Init on Page Load ===
-window.onload = function () {
-    document.getElementById('navbar').innerHTML =
-        '<ul> <li><a href="index.html">Home</a></li>   <li><a>Photography</a></li>    <li><a href="ratings.html">Ratings</a></li> </ul>';
-    
-    
-
-}
