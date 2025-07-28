8b0850cc8c4ca145da80e76c9ed2ef589cbd907c
Bobo <120399055+BosonChang@users.noreply.github.com>
2025-07-28 16:20:52 +0800

    Completed Table Tennis Page

---
+++ tabletennis/scripts/loadList.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('tabletennis/data/videos.json')
+        .then(response => response.json())
+        .then(posts => {
+            const container = document.getElementById('content-area');
+            posts.reverse().forEach((post, index) => {
+                const postElement = document.createElement('div');
+                postElement.className = 'post';
+                postElement.innerHTML = `
+                <iframe src="https://www.youtube.com/embed/${post.contentID}" allowfullscreen></iframe>
+          <div class="text-section">
+            <h2>${post.title}</h2>
+            <h4>${post.date}</h4>
+            <p>${post.notes}</p>
+          </div>
+        `;
+                container.appendChild(postElement);
+            });
+        })
+        .catch(error => {
+            console.error('Error loading posts:', error);
+        });
+});
